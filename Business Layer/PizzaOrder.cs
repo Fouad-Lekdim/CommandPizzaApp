26139a1193be2c78170989ee4b000df42be4619d
Fouad-LEKDIM <foufou_lekdim@outlook.com>
2026-04-24 15:58:10 +0200

    Add Business Layer and Data Access Layer projects to the solution

---
+++ Business Layer/PizzaOrder.cs
@@ -0,0 +1,111 @@
+﻿using PizzaDataAccess___Data_Layer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer
+{
+    public class clsPizzaOrder
+    {
+        public int OrderId { get; set; }
+        public string Toppings { get; set; }
+        public string Size { get; set; }
+        public int SizeId { get; set; }
+        public string CrustType { get; set; }
+        public int CrustTypeId { get; set; }
+        public string WhereToEat { get; set; }
+        public float TotalPrice { get; set; }
+
+        public clsPizzaOrder()
+        {
+            this.OrderId = -1;
+            this.Toppings = "";
+            this.Size = "";
+            this.SizeId = -1;
+            this.CrustType = "";
+            this.CrustTypeId = -1;
+            this.WhereToEat = "";
+            this.TotalPrice = 0;
+        }
+
+        public clsPizzaOrder(int orderId, int sizeId, int crustId, string toppings, 
+                string whereToEat, float totalPrice)
+        {
+            this.OrderId=orderId;
+            this.SizeId=sizeId;
+            this.CrustTypeId=crustId;
+            this.Toppings=toppings;
+            this.WhereToEat=whereToEat;
+            this.TotalPrice = totalPrice;
+        }
+
+        public static string[] _toppings = {"Extra Cheese", "Mashrooms", "Tomatoes", "Onion",
+                                "Olives", "Green Pepper"};
+
+        public const string thinCrust = "Thin";
+        public const string thickCrust = "Thick";
+        public const string toEatIn = "EatIn";
+        public const string toTakeAway = "TakeAway";
+
+        public const float _toppingUnitPrice = 2f;
+
+        public float UpdateSelectedSizePrice()
+        {
+            float sizePrice = 0f;
+            int sizeId = -1;
+            if (clsPizzaDataAccess.GetPizzaSizePrice(this.Size, ref sizeId, ref sizePrice))
+            {
+                this.SizeId = sizeId;
+                return sizePrice;
+            }
+            return 0;
+        }
+
+        public float UpdateSelectedCrustPrice()
+        {
+            float crustPrice = 0f;
+            int crustId = -1;
+            if (clsPizzaDataAccess.GetCrustTypePrice(this.CrustType, ref crustId, ref crustPrice))
+            {
+                this.CrustTypeId = crustId;
+                return crustPrice;
+            }
+            return 0;
+        }
+
+        public int AddNewOrder()
+        {
+            return clsPizzaDataAccess.AddNewPizzaOrder(this.SizeId, this.CrustTypeId, this.Toppings,
+                this.WhereToEat, this.TotalPrice);
+        }
+
+        public static DataTable GetAllPizzaOrders()
+        {
+            return clsPizzaDataAccess.GetAllPizzaOrders();
+        }
+
+        public static clsPizzaOrder FindPizzaOrder(int pizzaOrder)
+        {
+            int sizeId = -1, crustTypeId = -1;
+            string toppings = "", whereToEat = "";
+            float totalPrice = -1f;
+
+            if (clsPizzaDataAccess.GetPizzaOrderById(pizzaOrder, ref sizeId, ref crustTypeId,
+                    ref toppings, ref whereToEat, ref totalPrice))
+            {
+                return new clsPizzaOrder(pizzaOrder, sizeId, crustTypeId, toppings, whereToEat,
+                        totalPrice);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ PizzaDataAccess - Data Layer/PizzaDataAccess.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaDataAccess___Data_Layer
+{
+    public class clsPizzaDataAccess
+    {
+        public static bool GetPizzaSizePrice(string size, ref int sizeId, ref float price)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "SELECT * from PizzaSizes WHERE Size = @size;";
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            cmd.Parameters.AddWithValue("@size", size);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isFound = true;
+                    price = Convert.ToSingle(reader["Price"]);
+                    sizeId = (int)reader["PizzaSizeId"];
+                    reader.Close();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+                isFound=false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+        public static bool GetCrustTypePrice(string crustType, ref int crustTypeId, ref float crustPrice)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "SELECT * from CrustTypes WHERE CrustType = @CrustType;";
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            cmd.Parameters.AddWithValue("@CrustType", crustType);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isFound = true;
+                    crustPrice = Convert.ToSingle(reader["CrustPrice"]);
+                    crustTypeId = (int)reader["CrustId"];
+
+                    reader.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+        public static int AddNewPizzaOrder(int sizeId, int crustId, string toppings,
+            string whereToEat, float totalPrice)
+        {
+            int orderId = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = @"INSERT into Orders (PizzaSizeId, CrustTypeId, Toppings, WhereToEat,
+                             TotalPrice) values (@sizeId, @crustId, @toppings, @whereToEat, @totalPrice);
+                             SELECT SCOPE_IDENTITY();";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            cmd.Parameters.AddWithValue("@sizeId", sizeId);
+            cmd.Parameters.AddWithValue("@crustId", crustId);
+            cmd.Parameters.AddWithValue("@toppings", toppings);
+            cmd.Parameters.AddWithValue("@whereToEat", whereToEat);
+            cmd.Parameters.AddWithValue("@totalPrice", totalPrice);
+
+            try
+            {
+                connection.Open();
+                object result = cmd.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    orderId = insertedID;
+                }
+            }
+            catch (Exception)
+            {
+                ;
+            }
+            finally { 
+                connection.Close();            
+            }
+
+            return orderId;
+        }
+                
+        public static DataTable GetAllPizzaOrders()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "Select * from Orders;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                ;
+            }
+            finally
+            {
+                connection.Close(); 
+            }
+
+            return dt;
+        }
+
+        public static bool GetPizzaOrderById(int orderId, ref int pizzaSizeId, ref int CrustTypeId,
+                                  ref string toppings, ref string whereToEat, ref float totalPrice)
+        {
+            bool isFound = false;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "Select * from Orders Where OrderNumber = @orderid;";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            cmd.Parameters.AddWithValue("@orderid", orderId);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isFound = true;
+                    pizzaSizeId = (int)reader["PizzaSizeId"];
+                    CrustTypeId = (int)reader["CrustTypeId"];
+                    
+                    if (reader["Toppings"] != DBNull.Value)
+                    {
+                        toppings = (string)reader["Toppings"];
+                    }
+                    else
+                    {
+                        toppings = "";
+                    }
+
+                    whereToEat = (string)reader["WhereToEat"];
+                    totalPrice = Convert.ToSingle(reader["TotalPrice"]);
+
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+    }
+}
